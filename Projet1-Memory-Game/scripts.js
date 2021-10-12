4654b98731086b94c888fe1a37dd7b0a85ca212e
Cleiton1912 <cleiton.silvalima@outlook.fr>
2021-10-12 20:46:51 +0200

    init

--- Projet1-Memory-Game/scripts.js
+++ Projet1-Memory-Game/scripts.js
@@ -1,12 +1,25 @@
+/*
 function catesAleatoire(min, max){
     return Math.floor(Math.random()* (max-min+1)) +1;
 }
 
 const cards = [
-    
+    https://st2.depositphotos.com/2268777/11372/v/950/depositphotos_113728876-stock-illustration-cartoon-animals-for-kids-little.jpg,
+    https://st2.depositphotos.com/2268777/11372/v/950/depositphotos_113728876-stock-illustration-cartoon-animals-for-kids-little.jpg,
+    https://previews.123rf.com/images/idesign2000/idesign20001205/idesign2000120500109/13446433-dessin-anim%C3%A9-tigre.jpg,
+    https://previews.123rf.com/images/idesign2000/idesign20001205/idesign2000120500109/13446433-dessin-anim%C3%A9-tigre.jpg,
+    https://i2.wp.com/i.pinimg.com/originals/0c/57/00/0c570043868da000386bd7914019b3aa.jpg?ssl=1,
+    https://i2.wp.com/i.pinimg.com/originals/0c/57/00/0c570043868da000386bd7914019b3aa.jpg?ssl=1,
+    https://previews.123rf.com/images/zenfruitgraphics/zenfruitgraphics1509/zenfruitgraphics150900010/44984914-bande-dessin%C3%A9e-de-poisson-clown.jpg,
+    https://previews.123rf.com/images/zenfruitgraphics/zenfruitgraphics1509/zenfruitgraphics150900010/44984914-bande-dessin%C3%A9e-de-poisson-clown.jpg,
+    https://w7.pngwing.com/pngs/775/942/png-transparent-tiger-giant-panda-wildlife-illustration-lazy-panda-mammal-animals-carnivoran.png,
+    https://w7.pngwing.com/pngs/775/942/png-transparent-tiger-giant-panda-wildlife-illustration-lazy-panda-mammal-animals-carnivoran.png,
+    https://thumbs.dreamstime.com/b/dessin-anim%C3%A9-mignon-de-baleine-58047733.jpg,
+    https://thumbs.dreamstime.com/b/dessin-anim%C3%A9-mignon-de-baleine-58047733.jpg,
 ];
 
 
 for( let i = 0; i< cards.length; i++){
 
 };
+*/
