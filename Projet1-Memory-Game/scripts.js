b586e78d6df16bd618801ecaffae4bd2f1092ce0
Cleiton1912 <cleiton.silvalima@outlook.fr>
2021-10-14 00:16:07 +0200

    etape3

--- Projet1-Memory-Game/scripts.js
+++ Projet1-Memory-Game/scripts.js
@@ -1,25 +1,42 @@
-/*
-function catesAleatoire(min, max){
-    return Math.floor(Math.random()* (max-min+1)) +1;
-}
 
-const cards = [
-    https://st2.depositphotos.com/2268777/11372/v/950/depositphotos_113728876-stock-illustration-cartoon-animals-for-kids-little.jpg,
-    https://st2.depositphotos.com/2268777/11372/v/950/depositphotos_113728876-stock-illustration-cartoon-animals-for-kids-little.jpg,
-    https://previews.123rf.com/images/idesign2000/idesign20001205/idesign2000120500109/13446433-dessin-anim%C3%A9-tigre.jpg,
-    https://previews.123rf.com/images/idesign2000/idesign20001205/idesign2000120500109/13446433-dessin-anim%C3%A9-tigre.jpg,
-    https://i2.wp.com/i.pinimg.com/originals/0c/57/00/0c570043868da000386bd7914019b3aa.jpg?ssl=1,
-    https://i2.wp.com/i.pinimg.com/originals/0c/57/00/0c570043868da000386bd7914019b3aa.jpg?ssl=1,
-    https://previews.123rf.com/images/zenfruitgraphics/zenfruitgraphics1509/zenfruitgraphics150900010/44984914-bande-dessin%C3%A9e-de-poisson-clown.jpg,
-    https://previews.123rf.com/images/zenfruitgraphics/zenfruitgraphics1509/zenfruitgraphics150900010/44984914-bande-dessin%C3%A9e-de-poisson-clown.jpg,
-    https://w7.pngwing.com/pngs/775/942/png-transparent-tiger-giant-panda-wildlife-illustration-lazy-panda-mammal-animals-carnivoran.png,
-    https://w7.pngwing.com/pngs/775/942/png-transparent-tiger-giant-panda-wildlife-illustration-lazy-panda-mammal-animals-carnivoran.png,
-    https://thumbs.dreamstime.com/b/dessin-anim%C3%A9-mignon-de-baleine-58047733.jpg,
-    https://thumbs.dreamstime.com/b/dessin-anim%C3%A9-mignon-de-baleine-58047733.jpg,
-];
+const cards = document.querySelectorAll('.memory-card');
 
+let isCardFlipped = false;
+let firstCard, secondCard;
 
-for( let i = 0; i< cards.length; i++){
+function flipcard(){
+    this.classList.toggle("flip");
+    if (!isCardFlipped){
+        isCardFlipped = true;
+        firstCard = this;
+    } else {
+        isCardFlipped = false;
+        secondCard = this;
+        if (firstCard.dataset.name === secondCard.dataset.name){
+            firstCard.removeEventListener("click", flipcard);
+            secondCard.removeEventListener("click", flipcard);
+        } else {
+            setTimeout(()=>{
+                firstCard.classList.remove("flip");
+                secondCard.classList.remove("flip");
+            },1500);
+        }
+        }
+    }
+    cards.forEach((card)=> {
+    card.addEventListener("click", flipcard);
+    });
 
-};
-*/
+    let temps = 30
+    const timerElement = document.getElementById("timer")
+    timerElement.innerText = "29"
+
+    
+
+    function diminuerTemps() {
+        timerElement.innerText = temps
+        temps--
+    }
+
+    setInterval(diminuerTemps, 1000)
+    temps = temps <= 0 ? 0 : temps - 1
